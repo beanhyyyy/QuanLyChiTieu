190739fe9eef461b5933145c0272e317bca0995f
kanghy123 <48789249+kanghy123@users.noreply.github.com>
2019-04-25 14:39:56 +0700

    Update tableQLCTsControllerTest.cs

--- QLCT.Tests/Controllers/tableQLCTsControllerTest.cs
+++ QLCT.Tests/Controllers/tableQLCTsControllerTest.cs
@@ -30,7 +30,30 @@ namespace QLCT.Tests.Controllers
             //Co hien het list
             Assert.AreEqual(db.tableQLCTs.Count(), (result.Model as List<tableQLCT>).Count());
 
-            
+            var result0 = controller.Edit(0);
+
+            //Test Edit
+
+            //Test Edit khong ton tai
+            Assert.IsInstanceOfType(result0, typeof(HttpNotFoundResult));
+
+            //Khai bao item dau cua table
+            var item = db.tableQLCTs.First();
+
+            //Khai bao view va ep kieu thanh view cua edit id
+            var result1 = controller.Edit(item.id) as ViewResult;
+
+            //Test co hien dc result1
+            Assert.IsNotNull(result1);
+
+            //Khai bao model
+            var model = result1.Model as tableQLCT;
+
+            //Test model co hien dc
+            Assert.IsNotNull(model);
+
+            //Test id of item = id of model
+            Assert.AreEqual(item.id, model.id);
         }
     }
 }
